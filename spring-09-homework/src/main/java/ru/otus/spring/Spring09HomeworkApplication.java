120335911be080d80c7dc7160d2092c7bddda1ae
Alex Sokolov <rt.sokolov@gmail.com>
2022-07-19 01:05:28 +0300

    Homework 9

---
+++ spring-09-homework/src/main/java/ru/otus/spring/Spring09HomeworkApplication.java
@@ -0,0 +1,12 @@
+package ru.otus.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09HomeworkApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Spring09HomeworkApplication.class, args);
+    }
+}
