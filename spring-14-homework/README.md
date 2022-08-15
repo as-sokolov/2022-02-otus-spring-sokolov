9623b3baaaad9df60d143cdb091596b5e094f19a
Alex Sokolov <rt.sokolov@gmail.com>
2022-08-16 02:37:08 +0300

    Homework 14

---
+++ spring-14-homework/README.md
@@ -0,0 +1,3 @@
+otus.ru 2022-02 Spring Соколов Алексей
+
+spring-14-homework - Задание 14. Spring Batch 
---
+++ spring-14-homework/src/main/java/ru/otus/spring/models/rdbms/Comment.java
@@ -0,0 +1,35 @@
+package ru.otus.spring.models.rdbms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="comments_seq")
+    @SequenceGenerator(name="comments_seq", sequenceName="comments_seq", allocationSize = 1)
+    @Column(name="id")
+    private long id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
